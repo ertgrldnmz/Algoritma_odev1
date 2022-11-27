6f3732f15078cd3b063c9ebed736a24e82ed091a
çağrı dönmez <ertgrldnmz@outlook.com>
2022-11-27 16:23:01 +0300

    readme added, create a gitignore files.

---
+++ Program.cs
@@ -0,0 +1,101 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+         Methods methods=new Methods();
+        //1
+        methods.FirstMethod();
+
+        //2
+        methods.SecondMethod();
+        
+        //3
+        methods.ThirdMethod();
+
+        //4
+        methods.FourthMethod();
+
+        
+
+
+        
+        
+    }
+
+}
+public class  Methods
+{
+    public void FirstMethod()
+    {
+        Console.WriteLine("please,enter a positive number!: ");
+        int n=Convert.ToInt32(Console.ReadLine());
+        uint [] array=new uint[n];
+        
+        
+
+        for(int i=0;i<array.Length;i++)
+        {
+            Console.WriteLine("please, enter {0}. number:",i+1);
+            array[i]=Convert.ToUInt32(Console.ReadLine());         
+
+        }
+        foreach (var item in array)
+        {
+            if(item%2==0)
+            Console.WriteLine(item);
+                           
+        }
+    } 
+
+    public void SecondMethod()
+    {
+
+        Console.WriteLine("please, enter a number:");
+        int x=Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("please, enter a number:");
+         int y=Convert.ToInt32(Console.ReadLine());
+        int[] dizi=new int[x];
+        for(int i=0;i<x;i++)
+        {
+         Console.WriteLine("please, enter {0}. number:",i+1);
+          dizi[i]=Convert.ToInt32(Console.ReadLine());  
+        }
+
+         foreach (var item in dizi)
+        {
+            if(item%y==0||item==y)
+            Console.WriteLine(item);
+                           
+        }
+       
+    }
+     public void ThirdMethod()
+        {
+            Console.WriteLine("please,enter positive number");
+            int n=Convert.ToInt32(Console.ReadLine());  
+
+            string [] array=new string[n];
+            
+            for(int i=0;i<n;i++)
+            {
+                Console.WriteLine("please, enter {0}. word:",i+1);
+                array[i]=Console.ReadLine();
+            }
+            Console.WriteLine("reverse pronunciation:");
+            for (int i = n-1; i >= 0; i--)
+            {
+                Console.WriteLine(array[i]);
+            }
+        }
+       
+        public void FourthMethod()
+        {
+            Console.WriteLine("Please enter a sentence.");
+            string s=Console.ReadLine();            
+            string [] array=s.Split(" ");
+            string word=string.Join("",array);
+            Console.WriteLine("Word Count:"+array.Count()+"   Letter Count:"+word.Count());
+            
+
+        }
+}
---
+++ readme.md
@@ -0,0 +1,13 @@
+## Algoritma Soruları
+#### Ödev-1
+
+- **Soru1:** Bir konsol uygulamasında kullanıcıdan pozitif bir sayı girmesini isteyin(n). Sonrasında kullanıcıdan n adet pozitif sayı girmesini isteyin. Kullanıcının girmiş olduğu sayılardan çift olanlar console'a yazdırın.
+
+- **Soru2:** Bir konsol uygulamasında kullanıcıdan pozitif iki sayı girmesini isteyin (n, m). Sonrasında kullanıcıdan n adet pozitif sayı girmesini isteyin. Kullanıcının girmiş olduğu sayılardan m'e eşit yada tam bölünenleri console'a yazdırın.
+
+- **Soru3:** Bir konsol uygulamasında kullanıcıdan pozitif bir sayı girmesini isteyin (n). Sonrasında kullanıcıdan n adet kelime girmesi isteyin. Kullanıcının girişini yaptığı kelimeleri sondan başa doğru console'a yazdırın.
+
+- **Soru4:** Bir konsol uygulamasında kullanıcıdan bir cümle yazması isteyin. Cümledeki toplam kelime ve harf sayısını console'a yazdırın.
+
+[www.patika.dev](https://www.patika.dev/tr)
+
